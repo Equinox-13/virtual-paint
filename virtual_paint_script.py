a88407a33f0d87286f6fc48d11cc98d4bf68663b
Equinox-13 <quaid.johar33@gmail.com>
2021-05-03 01:25:09 +0530

    Add contours method to detect the color

--- virtual_paint_script.py
+++ virtual_paint_script.py
@@ -15,8 +15,8 @@ cap.set(4, frameHeight)
 # Sets brightness of screen with id 10
 cap.set(10, 150)
 
-# Orange Purple Green
-myColors = [[5,107,0,19,255,255], [133,5,0,159,156,255], [57,76,0,100,255,255]]
+# Orange Blue Green
+myColors = [[0,103,0,161,255,255], [83,90,58,179,249,255], [10,83,187,179,249,255]]
 
 def findColor(img, myColors):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -25,11 +25,25 @@ def findColor(img, myColors):
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(imgHSV, lower, upper)
-        cv2.imshow(str(color[0]), mask)
+        getContours(mask)
+        # cv2.imshow(str(color[0]), mask)
+
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print("area===========>",area)    
+        if area > 500:
+            cv2.drawContours(imgResult, cnt, -1, (255,0,0), 3)
+            perimeter = cv2.arcLength(cnt, True)
+            print("perimeter---------->", perimeter)
+            approx = cv2.approxPolyDP(cnt, 0.02*perimeter, True)
+            x, y, w, h = cv2.boundingRect(approx)
 
 while True:
     success, img = cap.read()
+    imgResult = img.copy()
     findColor(img, myColors)
-    cv2.imshow("Video", img)
+    cv2.imshow("Video", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
